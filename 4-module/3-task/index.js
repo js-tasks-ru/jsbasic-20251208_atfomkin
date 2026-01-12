490cf67323b3f0a07587bc068afcd4609b6cb96a
Aleksandr Fomkin <atfomkin@avito.ru>
2026-01-12 09:11:06 +0300

    Задачи 4 и 5 модулей

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,33 @@
 function highlight(table) {
-  // ваш код...
+  const rows = table.tBodies[0].rows;
+
+  for (let row of rows) {
+    const cells = row.cells;
+
+    const age = Number(cells[1].textContent);
+    const gender = cells[2].textContent;
+    const statusCell = cells[3];
+
+    if (statusCell.hasAttribute('data-available')) {
+      const available = statusCell.getAttribute('data-available');
+
+      if (available === 'true') {
+        row.classList.add('available');
+      } else if (available === 'false') {
+        row.classList.add('unavailable');
+      }
+    } else {
+      row.hidden = true;
+    }
+
+    if (gender === 'm') {
+      row.classList.add('male');
+    } else if (gender === 'f') {
+      row.classList.add('female');
+    }
+
+    if (age < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
