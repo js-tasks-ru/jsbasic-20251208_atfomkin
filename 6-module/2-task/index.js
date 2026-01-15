6baa6fde31c57c29143e91cbe07231083c3b4d42
Aleksandr Fomkin <atfomkin@avito.ru>
2026-01-15 18:00:44 +0300

    Задачи 6 модуля

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,35 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
   constructor(product) {
+    this.product = product;
+
+    this.elem = createElement(`
+      <div class="card">
+        <div class="card__top">
+          <img
+            src="/assets/images/products/${product.image}"
+            class="card__image"
+            alt="product"
+          >
+          <span class="card__price">€${product.price.toFixed(2)}</span>
+        </div>
+        <div class="card__body">
+          <div class="card__title">${product.name}</div>
+          <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+    `);
+
+    this.elem.querySelector('.card__button').addEventListener('click', () => {
+      this.elem.dispatchEvent(
+        new CustomEvent('product-add', {
+          detail: this.product.id,
+          bubbles: true
+        })
+      );
+    });
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,5 +3,75 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.currentSlide = 0;
+
+    this.elem = createElement(`
+      <div class="carousel">
+        <div class="carousel__arrow carousel__arrow_left">
+          <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__arrow carousel__arrow_right">
+          <img src="/assets/images/icons/angle-right-icon.svg" alt="icon">
+        </div>
+
+        <div class="carousel__inner">
+          ${this.slides.map(slide => `
+            <div class="carousel__slide" data-id="${slide.id}">
+              <img
+                src="/assets/images/carousel/${slide.image}"
+                class="carousel__img"
+                alt="slide"
+              >
+              <div class="carousel__caption">
+                <span class="carousel__price">€${slide.price.toFixed(2)}</span>
+                <div class="carousel__title">${slide.name}</div>
+                <button type="button" class="carousel__button">
+                  <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+                </button>
+              </div>
+            </div>
+          `).join('')}
+        </div>
+      </div>
+    `);
+
+    this.inner = this.elem.querySelector('.carousel__inner');
+    this.leftArrow = this.elem.querySelector('.carousel__arrow_left');
+    this.rightArrow = this.elem.querySelector('.carousel__arrow_right');
+
+    this.updateArrows();
+
+    this.rightArrow.addEventListener('click', () => {
+      this.currentSlide++;
+      this.moveCarousel();
+    });
+
+    this.leftArrow.addEventListener('click', () => {
+      this.currentSlide--;
+      this.moveCarousel();
+    });
+
+    this.elem.addEventListener('click', (event) => {
+      let button = event.target.closest('.carousel__button');
+      if (!button) return;
+
+      let slide = button.closest('.carousel__slide');
+
+      this.elem.dispatchEvent(new CustomEvent('product-add', {
+        detail: slide.dataset.id,
+        bubbles: true
+      }));
+    });
+  }
+
+  moveCarousel() {
+    this.inner.style.transform = `translateX(-${this.currentSlide * this.inner.offsetWidth}px)`;
+    this.updateArrows();
+  }
+
+  updateArrows() {
+    this.leftArrow.style.display = this.currentSlide === 0 ? 'none' : '';
+    this.rightArrow.style.display =
+      this.currentSlide === this.slides.length - 1 ? 'none' : '';
   }
 }
